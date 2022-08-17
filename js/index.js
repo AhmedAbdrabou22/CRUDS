c5d0a4109e1f0f6fd10c3f4cc471428f9015ed69
Ahmed <ahmedabdrabou723@gmail.com>
2022-08-17 16:33:48 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -39,29 +39,4 @@ submit.addEventListener('click',()=>{
     }
     dataProduct.push(newProduct);
     localStorage.setItem('product',JSON.stringify(dataProduct)); 
-    displayContent();
 })
-function displayContent(){
-    let tr = ''
-    for(let i = 0 ; i < dataProduct.length ; i++){
-        tr +=`
-            <tr>
-            <td>${i+1}</td>
-            <td>${dataProduct[i].title}</td>
-            <td>${dataProduct[i].price}</td>
-            <td>${dataProduct[i].taxes}</td>
-            <td>${dataProduct[i].ads}</td>
-            <td>${dataProduct[i].discount}</td>
-            <td>${dataProduct[i].total}</td>
-            <td>${dataProduct[i].cat}</td>
-            <td><button class="upload">Upload</button></td>
-            <td><button class="delete">Delete</button></td>
-            </tr>
-        `
-    }
-    document.getElementById('tbody').innerHTML = tr;
-}
-displayContent();
-
-let upload  = document.querySelectorAll('.upload');
-console.log(upload);
